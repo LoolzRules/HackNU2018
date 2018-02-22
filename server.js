7ead65039221632f71b93583495f0f28c7b92b30
LoolzRules <loolzrules@gmail.com>
2018-02-23 05:39:16 +0600

    Hotfix

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const body_parser = require( 'body-parser' );
 const info = require( './info.json' );
 const app = express();
 
+const port = process.env.PORT || 8082;
+
 const responses = {};
-for (let section in info.content_sections.sections) {
+info.content_sections.sections.forEach( ( section ) => {
   responses[section.name] = {};
-  for (let problem in section.problems) {
+  section.problems.forEach( ( problem ) => {
     responses[section.name][problem.name] = 0;
-  }
-}
+  } );
+} );
 
 app.use( body_parser.json() );
 app.use( body_parser.urlencoded( { extended: true } ) );
@@ -21,13 +23,13 @@ app.set( 'view engine', 'pug' );
 
 app.post( '/', ( req, res ) => {
   let response = req.body;
+  console.log( response, responses[response.section][response.problem] );
   responses[response.section][response.problem]++;
-
   res.send( "ok" );
 } );
 
 app.get( '/', ( req, res ) => {
-  res.render( 'main.pug', info, { responses: responses } );
+  res.render( 'main.pug', { info: info, responses: responses } );
 } );
 
-app.listen( 8082, () => console.log( 'Example app listening on port 8082!' ) );
+app.listen( port, () => console.log( `Example app listening on port ${port}` ) );
