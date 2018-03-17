1aa7c24b1408c0dea431b762282d762d43ee5036
LoolzRules <loolzrules@gmail.com>
2018-03-17 11:00:43 +0600

    Minor design changes, added ability to attach files

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require( 'express' );
 const body_parser = require( 'body-parser' );
 const info = require( './info.json' );
+const participantList = require( './participants.json' );
+
+const fs = require('fs');
+
 const app = express();
-
 const port = process.env.PORT || 8082;
-
-const responses = {};
-info.content_sections.sections.forEach( ( section ) => {
-  responses[section.name] = {};
-  section.problems.forEach( ( problem ) => {
-    responses[section.name][problem.name] = 0;
-  } );
-} );
 
 app.use( body_parser.json() );
 app.use( body_parser.urlencoded( { extended: true } ) );
@@ -19,17 +14,29 @@ app.use( '/imgs', express.static( 'imgs' ) );
 app.use( '/styles', express.static( 'styles' ) );
 app.use( '/views', express.static( 'views' ) );
 app.use( '/fonts', express.static( 'fonts' ) );
+app.use( '/files', express.static( 'files' ) );
 app.set( 'view engine', 'pug' );
 
 app.post( '/', ( req, res ) => {
   let response = req.body;
-  console.log( response, responses[response.section][response.problem] );
-  responses[response.section][response.problem]++;
+  console.log( response );
+
+  participantList[response.section][+response.problem -1]++;
   res.send( "ok" );
 } );
 
 app.get( '/', ( req, res ) => {
-  res.render( 'main.pug', { info: info, responses: responses } );
+  res.render( 'main.pug', { info: info, responses: participantList } );
 } );
 
 app.listen( port, () => console.log( `Example app listening on port ${port}` ) );
+
+
+setInterval(
+    () => {
+        fs.writeFile("./participants.json", JSON.stringify(participantList, null, 2), function (err) {
+            if (err) return console.log(err);
+            console.log('saved current progress');
+        });
+    }, 30000
+);
